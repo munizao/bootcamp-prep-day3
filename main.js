839ef98adeca5a701fa0263a83c23552f1b89703
Alexandre Muniz <aom@puzzlezapper.com>
2019-01-16 20:41:54 -0800

    even or odd input box

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
-var sum = 3 + 4;
-console.log('hello console', sum);
+var x = 8;
+console.log('hello console', x);
+if(x > 12) {
+    console.log(x);
+}    
+else {
+    console.log('x too small');
+}
+
+for(var i = 0; i < x; i++) {
+    if(i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+function sayHello() {
+    console.log('hi');
+}
+
+if(x % 2 === 0) {
+    sayHello();
+}
+//eslint-disable-next-line
+function evenOrOdd() {
+    var input = document.getElementById('number');
+    var y = input.value;
+    console.log(input.value);
+    if(y % 2 === 0) {
+        console.log('even');
+    }
+    else {
+        console.log('odd');
+    }
+}
